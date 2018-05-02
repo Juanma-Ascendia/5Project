4df83ae9c82087a31bdf8dd5b4b34557caa74714
Joseascendia <38324287+Joseascendia@users.noreply.github.com>
2018-05-02 20:26:10 +0200

    joseheader

---
+++ includes/header.php
@@ -0,0 +1,19 @@
+
+<header>
+<div class="franja-j">
+	<div class="container">
+	<div class="logo_J">
+		<a href="">Apache Friends</a>
+	</div>
+	<nav class="menu_J">
+		<ul>
+			<li ><a href="applications.php">Aplication</a></li>
+			<li ><a href="faq.php">FAQs</a></li>
+			<li ><a href="guides.php">HOW-TO Guides</a></li>
+			<li ><a href="info.php">PHPinfo</a></li>
+			<li ><a href="phpmyadmin.php">phpMyAdmin</a></li>
+		</ul>
+	</nav>
+</div>
+</div>
+</header>
