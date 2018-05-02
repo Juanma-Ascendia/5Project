f511865bbd38540e38d1b5655b2c81a9a2943f41
ascendiacarlo <elgrangasbich@gmail.com>
2018-05-02 20:21:04 +0200

    carlos

---
+++ guides.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+  <!-- Basic Page Needs
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <meta charset="utf-8">
+  <title>Your page title here :)</title>
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <!-- Mobile Specific Metas
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- FONT
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  
+
+  <!-- CSS
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/skeleton.css">
+  <link rel="stylesheet" href="css/guides.css">
+  <!-- Favicon
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link rel="icon" type="image/png" href="images/favicon.png">
+
+</head>
+<body>
+
+  <!-- Primary Page Layout
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <?php include 'includes/header.php'?>
+  <div class="gris-c">
+    <div class="letras-c"><h1><b>Windows</b> HOW-TO Guides</h1>
+  </div>
+  <div class="container">
+    <div class="row">
+      <div class="one-half column" style="margin-top: 5%">
+        <div class="frases-c">
+        <h5><a href="#">Backup and Restore MySQL/MariaDB Databases</a></h5>
+        <h5><a href="#">Configure FTP Access</a></h5>
+        <h5><a href="#">Configure Virtual Hosts</a></h5>
+        <h5><a href="#">Configure Wildcard-Based Subdomains</a></h5>
+        <h5><a href="#">Deploy an Application Using Git</a></h5>
+        <h5><a href="#">Get Started Quickly with WordPress</a></h5>
+        <h5><a href="#">Reset the MySQL/MariaDB Root Password</a></h5>
+        <h5><a href="#">Send Mail with PHP</a></h5>
+        <h5><a href="#">Use SQLite with PHP</a></h5>
+        <h5><a href="#">Activate and Use Xdebug</a></h5>
+        <h5><a href="#">Start a New Zend Framework 1.x Project</a></h5>
+        <h5><a href="#">Start a New Zend Framework 2.x Project</a></h5>
+        <h5><a href="#">Troubleshoot Apache Startup Problems</a></h5>
+        <h5><a href="#">Change the MySQL Temporary Files Directory</a></h5>
+        <h5><a href="#">Increase the PHP File Upload Limit</a></h5>
+        <h5><a href="#">Enable Remote Access to phpMyAdmin</a></h5>
+        <h5><a href="#">Use a Different PHP Version</a></h5>
+        <h5><a href="#">Run PHP under FastCGI</a></h5>
+      </div>
+    </div>
+    </div>
+  </div>
+  <?php include'includes/footer.php'?>
+<!-- End Document
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+</body>
+</html>
