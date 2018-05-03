c96e0844f76f6efed54f00fb9b2c3cc62799eb5e
valm <ascendia.valentinvm@gmail.com>
2018-05-03 17:53:58 +0200

    irene

---
+++ applications.php
@@ -0,0 +1,66 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+  <!-- Basic Page Needs
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <meta charset="utf-8">
+  <title>Applications/title>
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <!-- Mobile Specific Metas
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- FONT
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  
+
+  <!-- CSS
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/skeleton.css">
+  <link rel="stylesheet" href="css/style.css">
+
+
+  <!-- Favicon
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link href="images/favicon-xampp.png" rel="icon" type="image/png" />
+
+</head>
+<body>
+
+<?php include 'includes/header.php'; ?>
+
+
+
+	<div id="cuerpo">
+		<div id="text1">
+
+			<p>Apache Friends and Bitnami are cooperating to make dozens of open source applications available on XAMPP, for free. Bitnami-packaged applications include Wordpress, Drupal, Joomla! and dozens of others and can be deployed with one-click installers.Visit the <a href="https://bitnami.com/stack/xampp?utm_source=bitnami&utm_medium=installer&utm_campaign=XAMPP%2BModule">Bitnami XAMPP page</a> for details on the currently available apps.</p>
+
+			<br>
+
+			<p>Check out our  <a href="https://www.apachefriends.org/bitnami_for_xampp.html">Bitnami for XAMPP Start Guide</a> for more information about the applications installed.</p>
+			
+			<br>			
+
+				<p style="background: #FFFACD;  border: 1px solid #FF6347; padding: 5px">We are working on making Bitnami add-ons available for PHP 7, it is possible the current add-ons do not work well with this version.</p>
+
+
+		</div>
+
+		
+	</div>
+
+	<?php include 'includes/footer.php' ?>
+	
+
+  </body>
+
+
+
+
+</html>
