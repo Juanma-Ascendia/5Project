17c83acdefa2be17ee3872670c69dc1554eb109a
ascendiacarlo <elgrangasbich@gmail.com>
2018-05-03 18:59:49 +0200

    carlos

--- faq.php
+++ faq.php
@@ -38,43 +38,43 @@
 </div>
   <div class="container">
     <div class="row">
-      <div class="twelve column" style="margin-top: 5%">
+      <div class="twelve columns" style="margin-top: 5%">
         <div class="frases-cl">
-        <h5><a href="#">How do I install XAMPP?</a></h5>
-        <h5><a href="#">Does XAMPP include MySQL or MariaDB?</a></h5>
-        <h5><a href="#">How can I start XAMPP without setup?</a></h5>
-        <h5><a href="#">How do I start and stop XAMPP?</a></h5>
-        <h5><a href="#">How can I test that everything worked?</a></h5>
-        <h5><a href="#">How can I install a server as a service?</a></h5>
-        <h5><a href="#">Is XAMPP production ready?</a></h5>
-        <h5><a href="#">How do I uninstall XAMPP?</a></h5>
-        <h5><a href="#">What is the "lite" version of XAMPP?</a></h5>
-        <h5><a href="#">Where should I place my web content?</a></h5>
-        <h5><a href="#">Can I move the XAMPP installation?</a></h5>
-        <h5><a href="#">What are "Automatic start pages" for the WWW directories?</a></h5>
-        <h5><a href="#">Where can I change the configuration?</a></h5>
-        <h5><a href="#">Why can't XAMPP work on Windows XP SP2?</a></h5>
-        <h5><a href="#">Why doesn't XAMPP work on Vista?</a></h5>
-        <h5><a href="#">How do I check the md5 checksum?</a></h5>
-        <h5><a href="#">Why have changes in my php.ini not taken effect?</a></h5>
-        <h5><a href="#">Help! There is a virus in XAMPP!</a></h5>
-        <h5><a href="#">How do I configure my antivirus application?</a></h5>
-        <h5><a href="#">Why doesn't the Apache server start on my system?</a></h5>
-        <h5><a href="#">Why is my CPU load for Apache almost 99%?</a></h5>
-        <h5><a href="#">Why are pictures and style-sheets not displayed?</a></h5>
-        <h5><a href="#">How can I set a root password in MySQL?</a></h5>
-        <h5><a href="#">Can I use my own MySQL server?</a></h5>
-        <h5><a href="#">How do I restrict access to phpMyAdmin from the outside?</a></h5>
-        <h5><a href="#">How do I enable access to phpMyAdmin from the outside?</a></h5>
-        <h5><a href="#">Where is the IMAP support for PHP?</a></h5>
-        <h5><a href="#">Why don't some PHP open source apps work with XAMPP on Windows?</a></h5>
-        <h5><a href="#">Can I delete the "install" directory after installation?</a></h5>
-        <h5><a href="#">How do I activate the eaccelerator?</a></h5>
-        <h5><a href="#">How do I fix a connection error to my MS SQL server?</a></h5>
-        <h5><a href="#">How do I work with the PHP mcrypt extension?</a></h5>
-        <h5><a href="#">Do Microsoft Active Server Pages (ASP) work with XAMPP?</a></h5>
-        <h5><a href="#">How can I get XAMPP working on port 80 under Windows 10?</a></h5>
-        <h5><a href="#">How can I use Microsoft Edge to access local addresses under Windows 10?</a></h5>
+        <h5 class="linea"><a href="#">How do I install XAMPP?</a></h5>
+        <h5 class="linea"><a href="#">Does XAMPP include MySQL or MariaDB?</a></h5>
+        <h5 class="linea"><a href="#">How can I start XAMPP without setup?</a></h5>
+        <h5 class="linea"><a href="#">How do I start and stop XAMPP?</a></h5>
+        <h5 class="linea"><a href="#">How can I test that everything worked?</a></h5>
+        <h5 class="linea"><a href="#">How can I install a server as a service?</a></h5>
+        <h5 class="linea"><a href="#">Is XAMPP production ready?</a></h5>
+        <h5 class="linea"><a href="#">How do I uninstall XAMPP?</a></h5>
+        <h5 class="linea"><a href="#">What is the "lite" version of XAMPP?</a></h5>
+        <h5 class="linea"><a href="#">Where should I place my web content?</a></h5>
+        <h5 class="linea"><a href="#">Can I move the XAMPP installation?</a></h5>
+        <h5 class="linea"><a href="#">What are "Automatic start pages" for the WWW directories?</a></h5>
+        <h5 class="linea"><a href="#">Where can I change the configuration?</a></h5>
+        <h5 class="linea"><a href="#">Why can't XAMPP work on Windows XP SP2?</a></h5>
+        <h5 class="linea"><a href="#">Why doesn't XAMPP work on Vista?</a></h5>
+        <h5 class="linea"><a href="#">How do I check the md5 checksum?</a></h5>
+        <h5 class="linea"><a href="#">Why have changes in my php.ini not taken effect?</a></h5>
+        <h5 class="linea"><a href="#">Help! There is a virus in XAMPP!</a></h5>
+        <h5 class="linea"><a href="#">How do I configure my antivirus application?</a></h5>
+        <h5 class="linea"><a href="#">Why doesn't the Apache server start on my system?</a></h5>
+        <h5 class="linea"><a href="#">Why is my CPU load for Apache almost 99%?</a></h5>
+        <h5 class="linea"><a href="#">Why are pictures and style-sheets not displayed?</a></h5>
+        <h5 class="linea"><a href="#">How can I set a root password in MySQL?</a></h5>
+        <h5 class="linea"><a href="#">Can I use my own MySQL server?</a></h5>
+        <h5 class="linea"><a href="#">How do I restrict access to phpMyAdmin from the outside?</a></h5>
+        <h5 class="linea"><a href="#">How do I enable access to phpMyAdmin from the outside?</a></h5>
+        <h5 class="linea"><a href="#">Where is the IMAP support for PHP?</a></h5>
+        <h5 class="linea"><a href="#">Why don't some PHP open source apps work with XAMPP on Windows?</a></h5>
+        <h5 class="linea"><a href="#">Can I delete the "install" directory after installation?</a></h5>
+        <h5 class="linea"><a href="#">How do I activate the eaccelerator?</a></h5>
+        <h5 class="linea"><a href="#">How do I fix a connection error to my MS SQL server?</a></h5>
+        <h5 class="linea"><a href="#">How do I work with the PHP mcrypt extension?</a></h5>
+        <h5 class="linea"><a href="#">Do Microsoft Active Server Pages (ASP) work with XAMPP?</a></h5>
+        <h5 class="linea"><a href="#">How can I get XAMPP working on port 80 under Windows 10?</a></h5>
+        <h5 class="linea"><a href="#">How can I use Microsoft Edge to access local addresses under Windows 10?</a></h5>
       </div>
     </div>
     </div>
