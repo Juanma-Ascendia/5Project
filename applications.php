350abf682206cc243900566f3c5d7512618c6ea7
valm <ascendia.valentinvm@gmail.com>
2018-05-03 17:55:51 +0200

    valentin

--- applications.php
+++ applications.php
@@ -6,7 +6,7 @@
   <!-- Basic Page Needs
   –––––––––––––––––––––––––––––––––––––––––––––––––– -->
   <meta charset="utf-8">
-  <title>Applications/title>
+  <title>Applications</title>
   <meta name="description" content="">
   <meta name="author" content="">
 
